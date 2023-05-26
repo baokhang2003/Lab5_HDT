d3b8cc9d97ded06c68c03232ac45dcea1fe3cff5
baokhang2003 <125106594+baokhang2003@users.noreply.github.com>
2023-05-26 10:04:52 +0700

    BT

---
+++ PhongMayTinh.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication19;
+
+/**
+ *
+ * @author TH
+ */
+class PhongMayTinh extends PhongHoc {
+    private int soMayTinh;
+
+    public PhongMayTinh(String maPhong, String dayNha, double dienTich, int soBongDen, int soMayTinh) {
+        super(maPhong, dayNha, dienTich, soBongDen);
+        this.soMayTinh = soMayTinh;
+    }
+
+    public int getSoMayTinh() {
+        return soMayTinh;
+    }
+
+    public void setSoMayTinh(int soMayTinh) {
+        this.soMayTinh = soMayTinh;
+    }
+
+    @Override
+    public String toString() {
+        return "PhongMayTinh{" +
+                "maPhong='" + getMaPhong() + '\'' +
+                ", dayNha='" + getDayNha() + '\'' +
+                ", dienTich=" + getDienTich() +
+                ", soBongDen=" + getSoBongDen() +
+                ", soMayTinh=" + soMayTinh +
+                '}';
+    }
+}
+
